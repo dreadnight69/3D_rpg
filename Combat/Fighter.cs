93ef60111432e24cf66fc4aa8631772a3e96fd02
dreadnight69 <zaharov.jur@icloud.com>
2020-09-17 23:38:42 +0300

    scripts

---
+++ Combat/Fighter.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using RPG.Movement;
+using RPG.Core;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour, IAction
+    {
+        Health target;
+
+        [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
+
+        float timeSinceLastAttack = Mathf.Infinity;
+
+        private void Update()
+        {
+            timeSinceLastAttack += Time.deltaTime;
+
+            if(target == null) return;
+            if (target.IsDead()) return;
+
+            if (target != null && !GetIsInRange())
+            {
+                GetComponent<Mover>().MoveTo(target.transform.position, 1f);
+            }
+            else
+            {
+                GetComponent<Mover>().Cancel();
+                
+                AttackBehaviour();
+            }
+        }
+        public bool CanAttack(GameObject combatTarget)
+        {
+            if (combatTarget == null) { return false; }
+            Health targetToTest = combatTarget.GetComponent<Health>();
+            return targetToTest != null && !targetToTest.IsDead();
+
+        }
+        private void AttackBehaviour()
+        {
+            transform.LookAt(target.transform);
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
+                //this will trigger the Hit() event
+                TriggerAttack();
+                timeSinceLastAttack = 0;
+
+            }
+
+        }
+
+        private void TriggerAttack()
+        {
+            GetComponent<Animator>().ResetTrigger("stopAttack");
+            GetComponent<Animator>().SetTrigger("attack");
+        }
+
+        //Animation Event
+        void Hit()
+        {        
+            if(target == null) return;
+            target.TakeDamage(weaponDamage);
+        }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
+        }
+
+        public void Attack(GameObject combatTarget)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            target = combatTarget.GetComponent<Health>();
+                 
+        }
+
+        public void Cancel()
+        {
+            target = null;
+            StopAttack();
+            GetComponent<Mover>().Cancel();
+        }
+
+        private void StopAttack()
+        {
+            GetComponent<Animator>().ResetTrigger("attack");
+            GetComponent<Animator>().SetTrigger("stopAttack");
+        }
+
+    }
+}
---
+++ Movement/Mover.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using RPG.Core;
+using RPG.Saving;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour, IAction, ISaveable
+    {
+        NavMeshAgent navMeshAgent;
+        Health health;
+        
+        [SerializeField] float maxSpeed = 5.6f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            health = GetComponent<Health>();
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            navMeshAgent.enabled = !health.IsDead();
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination, float speedFraction)
+        {
+
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination, speedFraction);
+        }
+    
+        public void Cancel()
+        {
+            navMeshAgent.isStopped = true;
+
+        }
+
+        public void MoveTo(Vector3 destination, float speedFraction)
+        {
+            navMeshAgent.destination = destination;
+            navMeshAgent.speed = maxSpeed * Mathf.Clamp01(speedFraction);
+            navMeshAgent.isStopped = false;
+            
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMeshAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
+        public object CaptureState()
+        {
+            return new SerializableVector3(transform.position);
+        }
+
+        public void RestoreState(object state)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
